1abc66344829285f05c2439dc78310d8aa537587
alemjc <alemjc2204@gmail.com>
2015-12-16 21:20:51 -0500

    testing bestseller api.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+var NYTBESTSELLERURL = "http://api.nytimes.com/svc/books/v3/lists/best-sellers";
+
 $( document ).ready(function() {
 
 
   bootbox.prompt("Welcome to LiveCatalog, please enter your name?", function(result) {                
 	  console.log("Hi "+result);                          
 	});
+	getBestSellers();
 
 });
+
+function getBestSellers(){
+
+	$.ajax({
+		url: NYTBESTSELLERURL,
+		dataType:"json",
+		type:"GET",
+		success:function(data){
+			 console.log(data);
+		}
+
+	});
+
+}
+
