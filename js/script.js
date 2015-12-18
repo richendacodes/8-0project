91988901b57c046e7d350734b98f292f65b2b467
Richenda Gould <Richenda Gould>
2015-12-18 18:38:00 -0500

    committing for storage purposes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ $( document ).ready(function() {
 
 
   bootbox.prompt("Welcome to LiveCatalog, please enter your name?", function(result) {
+    var result = result;
+    if ($result.length <= 0)
+        console.log('Sorry');
+      else
+
     $('#hiUser').html("Hi, " + result)
 	  console.log("Hi "+result);                          
 	});
+
 
 });
